39df32fe220a29e9515cfd167279a8b1a19c7891
Victor <vick.orel@gmail.com>
2016-11-22 21:29:38 +0300

    Added tid

--- app/controllers/impressionist_controller.rb
+++ app/controllers/impressionist_controller.rb
@@ -51,6 +51,7 @@ module ImpressionistController
         :session_hash => session_hash,
         :ip_address => request.remote_ip,
         :referrer => request.referer,
+        :tid => cookies[:tid],
         :params => params_hash
         )
     end
